4d68146d9eec5232461eb279522563f464ebe616
Roman Cheplyaka <roma@ro-che.info>
2012-05-26 12:48:35 +0300

    Automatic commit

--- Cron.hs
+++ Cron.hs
@@ -20,7 +20,7 @@ checkTime currentTime time =
     getL minutes currentTime == tMin time &&
     getL hours currentTime == tHour time
 
-cron :: Time Int -> IO () -> IO ()
+cron :: Time NumOrStar -> IO () -> IO ()
 cron time action = do
     currentTime <- getZonedTime
     when (checkTime currentTime time) $
